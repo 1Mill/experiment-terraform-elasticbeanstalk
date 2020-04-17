bfed2aaf4dc71fddc105f0ea68f058a3012eaf93
1Mill <erik.ekberg@outlook.com>
2020-04-17 16:09:02 -0700

    On update, release new version

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,8 +25,17 @@ resource "aws_elastic_beanstalk_application" "eb" {
 resource "aws_elastic_beanstalk_environment" "env" {
 	application = aws_elastic_beanstalk_application.eb.name
 	description = "${var.PROJECT_NAME} web site (terraform-managed)"
+	version_label = aws_elastic_beanstalk_application_version.default.id
 
 	name = "production"
 	solution_stack_name = "64bit Amazon Linux 2018.03 v2.11.4 running Ruby 2.6 (Puma)"
 	tier= "WebServer"
+}
+resource "aws_elastic_beanstalk_application_version" "default" {
+	application = aws_elastic_beanstalk_application.eb.name
+	bucket = aws_s3_bucket.app.id
+	key = aws_s3_bucket_object.app.id
+	name = timestamp()
+
+	description = "Application version (terraform-managed)"
 }
