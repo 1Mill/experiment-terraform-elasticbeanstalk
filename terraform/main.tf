dfdc23b019bb88727912129f13922dad0b7059e8
1Mill <erik.ekberg@outlook.com>
2020-04-17 15:47:45 -0700

    Move content around

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,3 @@
-resource "aws_elastic_beanstalk_application" "eb" {
-	description = "${var.PROJECT_NAME} application (terraform-managed)"
-	name = var.PROJECT_NAME
-}
-resource "aws_elastic_beanstalk_environment" "env" {
-	application = aws_elastic_beanstalk_application.eb.name
-	description = "${var.PROJECT_NAME} web site (terraform-managed)"
-
-	name = "production"
-	solution_stack_name = "64bit Amazon Linux 2018.03 v2.11.4 running Ruby 2.6 (Puma)"
-	tier= "WebServer"
-}
-
 
 variable "DIST_PATH" {
 	default = "dist.zip"
@@ -29,4 +16,17 @@ resource "aws_s3_bucket_object" "app" {
 	bucket = aws_s3_bucket.app.id
 	key="app.zip"
 	source = var.DIST_PATH
+}
+
+resource "aws_elastic_beanstalk_application" "eb" {
+	description = "${var.PROJECT_NAME} application (terraform-managed)"
+	name = var.PROJECT_NAME
+}
+resource "aws_elastic_beanstalk_environment" "env" {
+	application = aws_elastic_beanstalk_application.eb.name
+	description = "${var.PROJECT_NAME} web site (terraform-managed)"
+
+	name = "production"
+	solution_stack_name = "64bit Amazon Linux 2018.03 v2.11.4 running Ruby 2.6 (Puma)"
+	tier= "WebServer"
 }
