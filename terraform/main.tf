ea6afa24640a5b7a86576a1e836acb2fdc909390
1Mill <erik.ekberg@outlook.com>
2020-04-16 19:06:41 -0700

    Add database port + update ENV vars

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,6 +7,9 @@ variable "DATABASE_ENGINE_VERSION" {
 variable "DATABASE_PASSWORD" {
 	type = string
 }
+variable "DATABASE_PORT" {
+	type = string
+}
 variable "DATABASE_USERNAME" {
 	type = string
 }
@@ -19,6 +22,7 @@ resource "aws_db_instance" "database" {
 	identifier = "${var.PROJECT_NAME}-database"
 	password = var.DATABASE_PASSWORD
 	username = var.DATABASE_USERNAME
+	vpc_security_group_ids = [aws_security_group.database.id]
 
 	allocated_storage = 100
 	auto_minor_version_upgrade = true
@@ -26,4 +30,23 @@ resource "aws_db_instance" "database" {
 	max_allocated_storage = 1000
 	name = "production"
 	skip_final_snapshot = true
+}
+resource "aws_security_group" "database" {
+	description = "RDS ${var.DATABASE_ENGINE} sg (terraform-managed)"
+	name = "${var.PROJECT_NAME}-database-sg"
+
+	ingress {
+		from_port = var.DATABASE_PORT
+		to_port = var.DATABASE_PORT
+
+		cidr_blocks = ["0.0.0.0/0"]
+		protocal = "tcp"
+	}
+
+	egress {
+		cidr_blocks = ["0.0.0.0/0"]
+		from_port = 0
+		protocal = "-1"
+		to_port = 0
+	}
 }
