f1058f5828ecf03d88e2d517be412d46853f1220
1Mill <erik.ekberg@outlook.com>
2020-04-16 17:27:51 -0700

    Create database

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,9 @@
+variable "DATABASE_ENGINE" {
+	type = string
+}
+variable "DATABASE_ENGINE_VERSION" {
+	type = string
+}
 variable "DATABASE_PASSWORD" {
 	type = string
 }
@@ -8,11 +14,14 @@ variable "PROJECT_NAME"{
 	type = string
 }
 resource "aws_db_instance" "database" {
-	auto_minor_version_upgrade = true
-	engine = "postgres"
-	engine_version = "11.6"
-	instance_class = "db.t3.micro"
-	name = "${var.PROJECT_NAME}-database"
+	engine = var.DATABASE_ENGINE
+	engine_version = var.DATABASE_ENGINE_VERSION
+	name = "${var.PROJECT_NAME}_database"
 	password = var.DATABASE_PASSWORD
 	username = var.DATABASE_USERNAME
+
+	allocated_storage = 100
+	auto_minor_version_upgrade = true
+	instance_class = "db.t3.micro"
+	max_allocated_storage = 1000
 }
