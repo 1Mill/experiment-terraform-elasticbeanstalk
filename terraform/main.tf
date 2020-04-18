8e6b7f57409e43e7c9ed37fd17167af939149e24
1Mill <erik.ekberg@outlook.com>
2020-04-18 10:55:51 -0700

    Organize main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,23 +1,9 @@
-
-variable "DIST_PATH" {
-	default = "dist.zip"
-	type = string
-}
-data "archive_file" "dist" {
-	output_path = var.DIST_PATH
-
-	source_dir = "./src"
-	type = "zip"
-}
+// Bucket to hold application versions
 resource "aws_s3_bucket" "app" {
 	bucket = "${var.PROJECT_NAME}-app"
 }
-resource "aws_s3_bucket_object" "app" {
-	bucket = aws_s3_bucket.app.id
-	key="app.zip"
-	source = var.DIST_PATH
-}
 
+// Elasticbeanstalk application
 resource "aws_elastic_beanstalk_application" "eb" {
 	description = "${var.PROJECT_NAME} application (terraform-managed)"
 	name = var.PROJECT_NAME
@@ -30,6 +16,23 @@ resource "aws_elastic_beanstalk_environment" "env" {
 	name = "production"
 	solution_stack_name = "64bit Amazon Linux 2018.03 v2.11.4 running Ruby 2.6 (Puma)"
 	tier= "WebServer"
+}
+
+// Create application version to run on Elasticbeanstalk application
+variable "DIST_PATH" {
+	default = "dist.zip"
+	type = string
+}
+data "archive_file" "dist" {
+	output_path = var.DIST_PATH
+
+	source_dir = "./src"
+	type = "zip"
+}
+resource "aws_s3_bucket_object" "app" {
+	bucket = aws_s3_bucket.app.id
+	key="app.zip"
+	source = var.DIST_PATH
 }
 resource "aws_elastic_beanstalk_application_version" "default" {
 	application = aws_elastic_beanstalk_application.eb.name
