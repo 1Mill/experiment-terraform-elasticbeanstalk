1e021cadaeced3ed0aef130504a5d6bda37d562c
1Mill <erik.ekberg@outlook.com>
2020-04-18 14:26:20 -0700

    Add key-pair to SSH into EC2 instances

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,6 +16,20 @@ resource "aws_elastic_beanstalk_environment" "env" {
 	name = "production"
 	solution_stack_name = "64bit Amazon Linux 2018.03 v2.11.4 running Ruby 2.6 (Puma)"
 	tier= "WebServer"
+
+	setting {
+		name = "EC2KeyName"
+		namespace = "aws:autoscaling:launchconfiguration"
+		value = aws_key_pair.generated_key.key_name
+	}
+}
+resource "tls_private_key" "example" {
+	algorithm = "RSA"
+	rsa_bits  = 4096
+}
+resource "aws_key_pair" "generated_key" {
+	key_name = "${var.PROJECT_NAME}-SSH-key"
+	public_key = tls_private_key.example.public_key_openssh
 }
 
 // Create application version to run on Elasticbeanstalk application
