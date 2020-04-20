6b35887c8493eaf91aed47656168b01a8f10b2c5
1Mill <erik.ekberg@outlook.com>
2020-04-19 20:52:17 -0700

    Hide non-IAC stuff

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,31 +1,31 @@
 // TODO: This should be seperate deploy-app service,
 // TODO: not part of the provisioning / IaC code.
-resource "aws_s3_bucket" "app" {
-	bucket = "${var.PROJECT_NAME}-app"
-}
-variable "DIST_PATH" {
-	default = "dist.zip"
-	type = string
-}
-data "archive_file" "dist" {
-	output_path = var.DIST_PATH
+// resource "aws_s3_bucket" "app" {
+// 	bucket = "${var.PROJECT_NAME}-app"
+// }
+// variable "DIST_PATH" {
+// 	default = "dist.zip"
+// 	type = string
+// }
+// data "archive_file" "dist" {
+// 	output_path = var.DIST_PATH
 
-	source_dir = "./src"
-	type = "zip"
-}
-resource "aws_s3_bucket_object" "app" {
-	bucket = aws_s3_bucket.app.id
-	key="${timestamp()}.zip"
-	source = var.DIST_PATH
-}
-resource "aws_elastic_beanstalk_application_version" "default" {
-	application = aws_elastic_beanstalk_application.eb.name
-	bucket = aws_s3_bucket.app.id
-	key = aws_s3_bucket_object.app.id
-	name = timestamp()
+// 	source_dir = "./src"
+// 	type = "zip"
+// }
+// resource "aws_s3_bucket_object" "app" {
+// 	bucket = aws_s3_bucket.app.id
+// 	key="${timestamp()}.zip"
+// 	source = var.DIST_PATH
+// }
+// resource "aws_elastic_beanstalk_application_version" "default" {
+// 	application = aws_elastic_beanstalk_application.eb.name
+// 	bucket = aws_s3_bucket.app.id
+// 	key = aws_s3_bucket_object.app.id
+// 	name = timestamp()
 
-	description = "Application version (terraform-managed)"
-}
+// 	description = "Application version (terraform-managed)"
+// }
 // TODO: ---
 
 resource "aws_elastic_beanstalk_application" "eb" {
@@ -35,7 +35,7 @@ resource "aws_elastic_beanstalk_application" "eb" {
 resource "aws_elastic_beanstalk_environment" "env" {
 	application = aws_elastic_beanstalk_application.eb.name
 	description = "${var.PROJECT_NAME} web site (terraform-managed)"
-	version_label = aws_elastic_beanstalk_application_version.default.id
+	// version_label = aws_elastic_beanstalk_application_version.default.id
 
 	name = "production"
 	solution_stack_name = "64bit Amazon Linux 2018.03 v2.11.4 running Ruby 2.6 (Puma)"
