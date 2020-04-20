9a8d2143e4d8039476a0d77c1b4b4b6f115cfe76
1Mill <erik.ekberg@outlook.com>
2020-04-20 11:30:17 -0700

    Release timestamped version

--- aws/entrypoint.sh
+++ aws/entrypoint.sh
@@ -1,3 +1,7 @@
 # !/bin/bash
+TIMESTAMP=`date +%Y-%m-%d_%H-%M-%S` && #
 cd repo && #
-git archive -o ../dist.zip HEAD:src #
+git archive -o ../${TIMESTAMP}.zip HEAD:src && #
+cd .. && #
+aws s3 cp ${TIMESTAMP}.zip s3://${PROJECT_NAME}-app/ --profile ${AWS_PROFILE} && #
+echo Released version: ${TIMESTAMP}
