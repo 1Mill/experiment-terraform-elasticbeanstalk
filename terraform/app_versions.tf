3d8caddb169f3f7f6bb179ed3754b0ba8a788f8c
1Mill <erik.ekberg@outlook.com>
2020-04-20 13:41:59 -0700

    Release specific version to terraform

--- terraform/app_versions.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_s3_bucket" "app" {
-	bucket = "${var.PROJECT_NAME}-app"
-}
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,17 @@
+variable "APP_VERSION"{
+	type = string
+}
+resource "aws_s3_bucket" "app" {
+	bucket = "${var.PROJECT_NAME}-app"
+}
+
+resource "aws_elastic_beanstalk_application_version" "default" {
+	application = aws_elastic_beanstalk_application.eb.name
+	bucket = aws_s3_bucket.app.id
+	key = "${var.APP_VERSION}.zip"
+	name = var.APP_VERSION
+}
+
 resource "aws_elastic_beanstalk_application" "eb" {
 	description = "${var.PROJECT_NAME} application (terraform-managed)"
 	name = var.PROJECT_NAME
@@ -5,7 +19,7 @@ resource "aws_elastic_beanstalk_application" "eb" {
 resource "aws_elastic_beanstalk_environment" "env" {
 	application = aws_elastic_beanstalk_application.eb.name
 	description = "${var.PROJECT_NAME} web site (terraform-managed)"
-	// version_label = aws_elastic_beanstalk_application_version.default.id
+	version_label = aws_elastic_beanstalk_application_version.default.id
 
 	name = "production"
 	solution_stack_name = "64bit Amazon Linux 2018.03 v2.11.4 running Ruby 2.6 (Puma)"
