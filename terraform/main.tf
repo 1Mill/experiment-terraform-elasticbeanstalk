59fba4e91d11e2fa0e6574287a81c24cca0700a7
1Mill <erik.ekberg@outlook.com>
2020-04-20 15:31:38 -0700

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,8 @@ variable "APP_VERSION"{
 }
 resource "aws_s3_bucket" "app" {
 	bucket = "${var.PROJECT_NAME}-app"
+
+	force_destroy = true
 }
 
 resource "aws_elastic_beanstalk_application_version" "default" {
