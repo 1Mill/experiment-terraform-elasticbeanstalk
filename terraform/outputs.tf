4fff006faebbf1970c373f8ea9154d86b9e08cff
1Mill <erik.ekberg@outlook.com>
2020-04-22 19:57:45 -0700

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,9 @@
 output "url" {
 	value = aws_elastic_beanstalk_environment.default.endpoint_url
 }
+
+// ? Need to access the EC2 instance? Uncomment this
+// ? and copy the private key to a .pem file.
+// output "private_key" {
+// 	value = tls_private_key.default.private_key_pem
+// }
